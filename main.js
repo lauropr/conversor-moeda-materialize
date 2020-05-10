b0adc609a290f3b7a34f35b0f1add28c9977fb20
Ribeiro Lauro <lauropribeiro@gmail.com>
2020-05-10 01:57:35 +0000

    a partir da web ide

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const ovly = {
 	conversor: {
-		
+
 		_resultado: null,
-		
-		domain: "api/",
-		
-		carregaListaDeMoedas: function(){
-			
-			function cb(resultado){
-				for(moeda in resultado.rates){
+
+		domain: "https://api.exchangeratesapi.io/",
+
+		carregaListaDeMoedas: function () {
+
+			function cb(resultado) {
+				for (moeda in resultado.rates) {
 					var o = new Option(moeda, moeda);
 					/// jquerify the DOM object 'o' so we can use the html method
 					$(o).html(moeda);
@@ -19,49 +19,49 @@ const ovly = {
 		},
 		chamarAPI: function (e) {
 			var dCotacao = $("#data_cotacao").val();
-			var endpoint = this.domain + (dCotacao?  dCotacao : "latest");
+			var endpoint = this.domain + (dCotacao ? dCotacao : "latest");
 			var oParametros = {
 				base: $("#moeda_origem").val()
 			};
-			
+
 			console.log("Chamando API...");
 			$.get(endpoint, oParametros, $.proxy(this.callback, this));
 		},
-		
+
 		callback: function (resultado) {
 			this._resultado = resultado;
 			this._preencheParagrafo();
 			this._preencheTabela();
-			
+
 		},
-		_preencheParagrafo: function(){
+		_preencheParagrafo: function () {
 			$("#resultado_moeda_base").text(this._resultado.base);
 			$("#resultado_data_cotacao").text(this._resultado.date);
 		},
-		
-		_preencheTabela: function(){
+
+		_preencheTabela: function () {
 			$("#tabela tbody").empty(); // limpa tabela
 			var aMoedasSelecionadas = $("#moeda_destino").val();
 			$.each(aMoedasSelecionadas, $.proxy(this._criaLinha, this));
 		},
-		_criaLinha: function(i,moeda){
-			
+		_criaLinha: function (i, moeda) {
+
 			// parseFloat(oResposta.rates[sMoedaDestino]).toFixed(2);
 			var fValor = this._resultado.rates[moeda];
 			fValor = parseFloat(fValor).toFixed(2);
-			
+
 			$("#tabela").find('tbody')
-			    .append($('<tr>')
-			        .append(
-			        	$('<td>').text(fValor),
-			        	$('<td>').text(moeda)
-			        )
-			    );
-			
+				.append($('<tr>')
+					.append(
+						$('<td>').text(fValor),
+						$('<td>').text(moeda)
+					)
+				);
+
 		}
 	}
 };
 
-window.onload = function(){
+window.onload = function () {
 	ovly.conversor.carregaListaDeMoedas();
 }
